7bae11d65d605c396637f28b4128d4e8dc94f21d
Caroline <gekniateh@gmail.com>
2024-05-19 23:42:05 +0800

    create login, email & pw states, signup-link

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,58 @@
+import { useState } from "react";
+import {Link} from 'react-router-dom';
+import './Login.css';
 
 
 function Login() {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        error: "",
+    });
+
+
+    function handleChange(e) {
+      setUserInfo({...userInfo, [e.target.name]: e.target.value, error: ""})
+    }
+
     return (
-        <div className="LoginPage">
-            <div className="LoginContainer">
-                <h2 className="LoginTitle">
+        <div className="Login-Page">
+            <div className="Login-Container">
+                <h2 className="Login-Title">
                     Login To Your Account
                 </h2>
+                <form className="Login-Form">
+                    <div email-input>
+                    <label>Email </label>
+                    <input
+                       type="email"
+                       name="email"
+                       placeholder="e.g john@email.com"
+                       required value={userInfo.email}
+                       onChange={handleChange} >
+                    </input>
+                    </div>
+                    <div className="password-input">
+                    <label>Password </label>
+                    <input
+                       type="password"
+                       name="password"
+                       placeholder="Enter Password "
+                       required value={userInfo.password}
+                       onChange={handleChange} >
+                    </input>
+                    </div>
+                    <div className="submit-button">
+                        <button type="submit">Login</button>
+                    </div>
+                    <div className="sign-up-link">
+                        <h5>Don't have an account?</h5>
+                        <Link to="/sign-up">Sign Up</Link>
+                    </div>
+
+                </form>
             </div>
+            <p className="error-message">&nbsp;{userInfo.error}</p>
         </div>
     )
 
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,7 +4,7 @@ import Login from "../../components/Login/Login";
 
 function LoginPage () {
     return (
-        <div>
+        <div className="Login-Page">
            <Login/>
         </div>
         )
