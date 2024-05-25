5280c5e5a3afe9df37e8b9db2cb6f1e3ce8c6629
Carolinetgn94 <gekniateh@gmail.com>
2024-05-25 21:35:21 +0800

    create getuser axios to BE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import React, {useEffect} from "react";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { beServer } from "./server";
 
 function App() {
+
+  useEffect(() => {
+    axios.get(`${beServer}/user/getuser`, {withCredentials:true}).then((res) => {
+      toast.success(res.data.message);
+    }).catch((err) => {
+      toast.error(err.response.data.message)
+    })
+  }, [])
+
+
   return (
     <div className="App">
       <ToastContainer />
