18daf772e877be3881a93bfd5e3b05572b7480aa
Carolinetgn94 <gekniateh@gmail.com>
2024-05-27 22:42:52 +0800

    Create hero banner section, use random img, link to /products

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,9 +1,14 @@
 import "./HeroBanner.css";
+import { Link } from "react-router-dom";
 
 function HeroBanner() {
-    return (
-        <div>HERO BANNER</div>
-    )
+  return (
+    <div className="banner1">
+      <Link to="/products">
+        <img src="https://img.freepik.com/free-vector/hand-drawn-flea-market-shopping-sale-banner-template_23-2149585836.jpg?w=1060&t=st=1716819283~exp=1716819883~hmac=aeabd36804815bfffa810423251939df5841e9b117f27aa234ae7b1199029cc6"></img>
+      </Link>
+    </div>
+  );
 }
 
 export default HeroBanner;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import HeroBanner from "../../components/HeroBanner/HeroBanner"
+import HeroBanner from "../../components/HeroBanner/HeroBanner";
+import "./HomePage.css"
 
-
-function HomePage () {
-    return (
-        <div className="Home-Page">
-           <Header />
-           <HeroBanner />
-        </div>
-        )
+function HomePage() {
+  return (
+    <div className="Home-Page">
+      <div className="headersection">
+        <Header />
+      </div>
+      <div className="herosection">
+        <HeroBanner />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
