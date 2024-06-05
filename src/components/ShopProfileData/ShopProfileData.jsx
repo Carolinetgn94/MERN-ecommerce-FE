9044cc832216e343140f6070b5526d7850b2bea5
Carolinetgn94 <gekniateh@gmail.com>
2024-06-05 20:05:41 +0800

    create shophomepage that shows shop information, with profiledata component that can edit shop and logout, logouthandler not done yet, shop profile data component can show seed data product card

--- src/components/ShopProfileData/ShopProfileData.jsx
+++ src/components/ShopProfileData/ShopProfileData.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { productData } from "../../seedData";
+import ProductCard from "../ProductCard/ProductCard";
 import "./ShopProfileData.css";
+import { Link } from "react-router-dom";
 
-function ShopProfileData() {
-    return(
-        <div>shop profile data</div>
-    )
+function ShopProfileData({ isOwner }) {
+  const [active, setActive] = useState(1);
+
+  return (
+    <div className="ShopProfileContainer">
+      <div className="shopProfileHeader">
+        <button className="shopProductsButton">My Shop Products</button>
+        {isOwner && (
+          <div className="goDashboard">
+            <Link to="/dashboard">
+              <button className="dashboardButton" onClick={() => setActive(1)}>
+                Go Dashboard
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <br />
+      <div className="shopProductsDisplay">
+        {productData &&
+          productData.map((i, index) => (
+            <ProductCard data={i} key={index} isShop={true} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default ShopProfileData;
