0478cefe1b7f2ad08c2556c7d836e1402bab7455
Carolinetgn94 <gekniateh@gmail.com>
2024-06-19 19:58:06 +0800

    change banner img

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,11 +1,12 @@
 import "./HeroBanner.css";
 import { Link } from "react-router-dom";
+import banner1 from "../../assets/Banner1.png";
 
 function HeroBanner() {
   return (
     <div className="banner1">
       <Link to="/products">
-        <img src="https://img.freepik.com/free-vector/hand-drawn-flea-market-shopping-sale-banner-template_23-2149585836.jpg?w=1060&t=st=1716819283~exp=1716819883~hmac=aeabd36804815bfffa810423251939df5841e9b117f27aa234ae7b1199029cc6"></img>
+        <img src={banner1}></img>
       </Link>
     </div>
   );
